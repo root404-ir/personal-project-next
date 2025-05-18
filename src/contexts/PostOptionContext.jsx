253d50442fcca25f37bb14b64bea2e0874c44f1b
root404-ir <mohammadrg360@gmail.com>
2025-05-19 01:10:48 +0330

    install typescript and fixed local storage

--- src/contexts/PostOptionContext.jsx
+++ src/contexts/PostOptionContext.jsx
@@ -5,20 +5,24 @@ import { createContext, useEffect, useState } from "react";
 export const PostOptionContext = createContext()
 
 export const PostOptionProvider = ({ children }) => {
-    const [fontSize, setFontSize] = useState(
-        localStorage.getItem('fontSize') || '24px'
-    )
-    const [fontWeight, setFontWeight] = useState(
-        localStorage.getItem('fontWeight') || 'noraml'
-    )
-    const [lineSpacing, setLineSpacing] = useState(
-        localStorage.getItem('lineSpacing') || '40px'
-    )
+    const [fontSize, setFontSize] = useState('24px')
+    const [fontWeight, setFontWeight] = useState('normal')
+    const [lineSpacing, setLineSpacing] = useState('40px')
 
     useEffect(() => {
-        localStorage.setItem('fontSize', fontSize)
-        localStorage.setItem('fontWeight', fontWeight)
-        localStorage.setItem('lineSpacing', lineSpacing)
+        if (typeof window !== 'undefined') {
+            setFontSize(localStorage.getItem('fontSize') || '24px')
+            setFontWeight(localStorage.getItem('fontWeight') || 'normal')
+            setLineSpacing(localStorage.getItem('lineSpacing') || '40px')
+        }
+    },[])
+
+    useEffect(() => {
+        if (typeof window !== 'undefined') {
+            localStorage.setItem('fontSize', fontSize)
+            localStorage.setItem('fontWeight', fontWeight)
+            localStorage.setItem('lineSpacing', lineSpacing)
+        }
     }, [fontSize, fontWeight, lineSpacing])
     return (
         <PostOptionContext.Provider value={{ fontSize, setFontSize, fontWeight, setFontWeight, lineSpacing, setLineSpacing }}>
