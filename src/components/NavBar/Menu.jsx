94dcb72253e6da638d740b532dd2e0fea9804380
root404-ir <mohammadrg360@gmail.com>
2025-05-21 18:46:22 +0330

    fix url navbar

--- src/components/NavBar/Menu.jsx
+++ src/components/NavBar/Menu.jsx
@@ -3,9 +3,9 @@ const Menu = () => {
     return (
         <>
             <li className='nav-item hover:transform hover:scale-110 transition-all'><Link href={'/'}>صفحه اصلی</Link></li>
-            <li className='nav-item hover:transform hover:scale-110 transition-all'><Link href={'blog'}>وبلاگ</Link></li>
+            <li className='nav-item hover:transform hover:scale-110 transition-all'><Link href={'/blog'}>وبلاگ</Link></li>
             <li className='nav-item hover:transform hover:scale-110 transition-all'><Link href={'https://t.me/mtjsBot'} target="_blank">ربات تلگرام</Link></li>
-            <li className='nav-item hover:transform hover:scale-110 transition-all'><Link href={'contact'}>تماس</Link></li>
+            <li className='nav-item hover:transform hover:scale-110 transition-all'><Link href={'/contact'}>تماس</Link></li>
         </>
     )
 }
