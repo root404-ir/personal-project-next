ce7ef562eb4fc8bae3287aae6b7eae3b01dfe7be
root404-ir <mohammadrg360@gmail.com>
2025-05-21 19:48:21 +0330

    set meta for search console verify

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,11 +5,21 @@ import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/NavBar/Navbar";
 import BreadCrumb from "@/components/BreadCrumb";
 
+export const metadata = {
+    title: 'وبلاگ شخصی محمد تفقدی',
+    description: 'وبسایت محمد تفقدی ',
+    verification: {
+        google: 'YqRFW-zvQbF2eLCVZlaIVRjbXI932nrXpls25hlBKEM',
+    },
+    icons: {
+        icon: '/fav.svg',
+    },
+};
 export default function RootLayout({ children }) {
 
     return (
         <html lang="en">
-            <link rel="icon" href="/fav.svg" type="image/x-icon"/>
+            <link rel="icon" href="/fav.svg" type="image/x-icon" />
             <body className="bg-blue-950 text-white">
                 <PostOptionProvider>
                     <MenuProvider>
