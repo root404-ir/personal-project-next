39d2874e9f68159c230d82931d2d382881669957
root404 <mohammadrg360@gmail.com>
2026-06-26 17:09:16 +0330

    add codingeditor page url

--- src/components/CodeEditor.jsx
+++ src/components/CodeEditor.jsx
@@ -4,6 +4,7 @@ import Lottie from 'lottie-react'
 import animationData from './codingAnimationIcon.json'
 import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import Link from 'next/link'
 const CodeEditor = () => {
     useEffect(() => {
         gsap.registerPlugin(ScrollTrigger)
@@ -31,9 +32,9 @@ const CodeEditor = () => {
                         <p className='text-xl'>شما عزیزان میتوانید در شرایطی که دسترسی به IDE های خودتون ندارید از کد ادیتور آنلاین ما استفاده کنید کد ادیتوری ساده که از زبان های html,css,js پشتیبانی میکند و میتواند نیاز های اولیه شمارا در شرایط خاص برطرف سازد.</p>
                     </div>
                     <div className='flex justify-center'>
-                        <button className='group hover:shadow-2xl hover:shadow-green-700 hover:scale-105 hover:bg-blue-900 transition-all delay-75 bg-green-400 w-[50%] py-2 px-4 rounded-2xl cursor-pointer border '>
-                            <span className='font-bold group-hover:px-16 transition-all tracking-widest bg-gray-900/30 delay-75 rounded-2xl py-2 px-4'>ورود به کد ادیتور</span>
-                        </button>
+                        <Link href={'https://hcj-online-code-editor.vercel.app/'} target='_blank' className='group hover:shadow-2xl hover:shadow-green-700 hover:scale-105 hover:bg-blue-900 transition-all delay-75 bg-green-400 w-[50%] py-2 px-4 rounded-2xl cursor-pointer border '>
+                            <span className='flex justify-center font-bold group-hover:px-16 transition-all tracking-widest bg-gray-900/30 delay-75 rounded-2xl py-2 px-4 '>ورود به کد ادیتور</span>
+                        </Link>
                     </div>
                 </div>
                 <div id='section_left'>
